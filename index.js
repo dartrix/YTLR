841711662fa9d6e01fceaa51b73c6c7e5330603b
dartrix <darwinramos2009@hotmail.com>
2017-03-27 15:38:19 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,71 @@
+var ffmpeg = require('fluent-ffmpeg');
+var fs = require('fs');
+var config = require('./config.json');
+
+
+
+var rtmp = config.youtubeURL + '/' + config.youtubeKey;
+var files = fs.readdirSync(config.musicfolder);
+var img = config.imagesfolder;
+
+for (var i in files) {
+  files[i] = files[i].slice(0, -4);
+}
+
+var number = 0;
+stream(number);
+
+
+function stream(i){
+  ffmpeg(`music/${files[i]}.mp3`).withVideoCodec('libx264').withAudioCodec('libmp3lame')
+  .withAudioChannels(2).withAudioBitrate('128k')
+  .addInput(img +'/image%d.png').size('1280x720').aspect('16:9').videoFilters({
+    filter: 'drawtext',
+    options: {
+      fontfile:'font/SourceSansPro-Regular.ttf',
+      text: files[i],
+      fontsize: 36,
+      fontcolor: 'white',
+      x: '(main_w/2-text_w/2)',
+      y: 50,
+      shadowcolor: 'black',
+      shadowx: 2,
+      shadowy: 2
+    }
+  }).loop()
+  .toFormat('flv')
+  .addOptions(['-preset veryfast',
+  '-g 50', '-keyint_min 60', '-maxrate 3000k','-b:v 512k', '-crf 25',
+  '-bufsize 3000k', '-ar 44100', '-pix_fmt yuv420p','-shortest','-tune zerolatency']).on('start', function(it){
+    return console.log('FFmpeg start with ' + it);
+  }).on('progress', function(progress){
+    return console.log('FFmpeg in '+progress.timemark);
+  }).on('end', function(){
+    number += 1;
+    if (number < files.length) {
+      stream(number);
+    } else{
+      number = 0;
+      stream(number);
+    }
+    return console.log('FFmpeg end.');
+  }).save(rtmp).on('error', function(err, stdout, stderr) {
+      console.log('Cannot process video: ' + err.message + '\n\n' + stdout + '\n\n'+ stderr);
+      number += 1;
+    if (number < files.length) {
+      stream(number);
+    } else{
+      number = 0;
+      stream(number);
+    }
+    });
+}
+
+
+
+
+
+// .withAudioBitrate('712k')
+// .withAudioFrequency(48000)
+// rtmp://a.rtmp.youtube.com/live2/vz2f-6m0p-yxdh-b0hw
+//{source: 'music/Vanilla - Swept Away.mp3'}
